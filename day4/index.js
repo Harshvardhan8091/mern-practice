e55a2685f384d61521d492c4355dd9d5967b504e
Harshvardhan8091 <148524500+Harshvardhan8091@users.noreply.github.com>
2026-01-24 12:53:37 +0530

    js datatypes

---
+++ day4/index.js
@@ -0,0 +1,22 @@
+
+// let x=1
+// let y=3
+// let z=x+y
+// console.log(z) 
+// x="harsh" 
+// x=null 
+// console.log(typeof x)
+// let a=20;
+// let b="20";
+// let d = a+b;
+// console.log(d);
+
+
+
+let username ={
+    firstname: "harsh",
+    age:18,
+    email: "harsh@gmail.com"
+}
+console.log(username)
+console.log(typeof (username))
