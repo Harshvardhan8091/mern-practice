bd87762ecc33bcd58e1ada0d6e0805b0084d8878
Harshvardhan8091 <148524500+Harshvardhan8091@users.noreply.github.com>
2026-01-27 15:54:49 +0530

    operators, if-else

---
+++ day5/revision.js
@@ -0,0 +1,33 @@
+////////////////////////////////////////////////
+// // let x,y,z = 10;
+// // console.log(x,y,x)
+
+// let x=10,y=10,z=10;
+// console(x,y,z)
+
+////////////////////////////////////////////////
+
+// let text = `we are" vik"`
+// console.log(text.length)
+// console.log(text.trim())
+// console.log("this is \'a\'")
+// console.log("we are \"vik\"")
+//  let total = ` Total: ${(Price*(1+ vat)).toFixed(2)}`
+//  console.log(total)
+
+////////////////////////////////////////////////
+
+// let headers = "template string";
+// let tags = ["template string","Javascript","es6"];
+// let html = `<h2>$ {headers}</h2><ul>`;
+// console.log(headers,tags,html)
+
+// for(const x of tags){
+//     html += `<li>${x}</li>`
+// }
+// html += `<ul>`;
+// console.log(html)
+
+////////////////////////////////////////////////
+
+//string functions
