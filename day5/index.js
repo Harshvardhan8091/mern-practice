2342c26edbca3a6bf9541d280037bd325d53a92f
Harshvardhan8091 <148524500+Harshvardhan8091@users.noreply.github.com>
2026-01-27 10:27:37 +0530

    operators, if-else

---
+++ day5/index.js
@@ -0,0 +1,20 @@
+// let count = null;
+// let total = count || 10;
+// console.log("total", total)
+
+// let count1 = 0;
+// let total1 = count1 ?? 10;
+// console.log("total",total1)
+
+//if-else
+
+// if(true){
+//     let person1 = "jay"
+//     const person2 = "kay"
+//     var person3 = "hay"
+// }
+// console.log(person3)
+// console.log(person1)
+// console.log(person2) //we can only access var variable outside other throw error.
+
+//LOOOOOPS
