27d0c402b7e4e200a57e3ea9e3dbeda339cf3e89
Harshvardhan8091 <148524500+Harshvardhan8091@users.noreply.github.com>
2026-01-28 12:54:37 +0530

    operators, if-else

---
+++ day6/function.js
@@ -0,0 +1,78 @@
+////////////////////////////////
+// function sum(a=2,b=4){
+//     return(a+b);
+// }
+// sum(5,6)
+
+//////////////////////////////////
+
+// x = findmax(1, 23, 123, 3, 44, 56);
+
+// function findmax() {
+//     let max = -Infinity;
+//     for (let i = 0; i < arguments.length; i++) {
+//         if (arguments[i] > max) {
+//             max = arguments[i];
+//         }
+//     }
+//     return max;
+// }
+
+// console.log(x);
+/////////////////////////////////////////
+
+// function multiply(a,b){
+//     return a * b;
+// }
+// c= multiply(4);
+// console.log(c)
+///////////////////////////////////
+
+// function myf(x,y){
+//     if(y == undefined){
+//         y=2;
+//     }
+//     return x * y;
+// }
+
+////////////////////////////////////
+
+// function sum(...args){
+//     let sum=0;
+//     for(let arg of args) sum+=arg;
+//     return sum;
+// }
+// let x = sum(1,10,23,343,2)
+// console.log(x)
+
+////////////////////////////////////
+
+// const x = function(a,b) {
+//     return a*b
+// }
+// c=function(2,3)
+// console.log(c)
+
+/////////////////////////////////////
+
+// const add = function sum(a,b){
+//     return a+b;
+// }
+// add(2,3)
+// console.log(add)
+
+//////////////////////////////////////
+
+// function run(fn){
+//     return fn();
+// }
+
+// const sayhello = function(){
+//     return "hello"
+    
+// }
+//  a=run(hello)
+//  console.log(a)
+
+//////////////////////////////////////
+
