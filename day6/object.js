eb873adeafd73edb34f7c2858634652cd988e09c
Harshvardhan8091 <148524500+Harshvardhan8091@users.noreply.github.com>
2026-01-28 15:23:15 +0530

    operators, if-else

--- day6/object.js
+++ day6/object.js
@@ -7,5 +7,24 @@
 // console.log(person)
 
 //////////////////////////////////
+//making an object array and making another object inside it.
+
+let students = [
+    {
+        id:{
+            regno : 123,
+        },
+        firstname : "kai",
+        age : "20",
+
+        id:{
+            regno : 124,
+        },
+        firstname : "rai",
+        age : "21",
 
 
+    }
+];
+console.log(students[0].firstname);
+console.log(students[1].age);
