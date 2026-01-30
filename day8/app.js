ffd7a89c3c68521cdbae6143adec2f73efc5eebb
Harshvardhan8091 <148524500+Harshvardhan8091@users.noreply.github.com>
2026-01-30 10:01:06 +0530

    reading image and copying

--- day8/app.js
+++ day8/app.js
@@ -1,12 +1,36 @@
+// const fs = require('fs')
+// const path = require('path')
+
+// const filePath = path.join(__dirname, 'note.txt')
+
+// fs.appendFile(filePath, "\nthis is a new line", (err) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log("data added successfully");
+// })
+
+
+// const fs = require('fs')
+// const path = require('path')
+
+// const imagePath = path.join(__dirname, 'java.jpg')
+
+// fs.readFile(imagePath, (err, data) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log("image read successfully");
+// })
+
 const fs = require('fs')
-const path = require('path')
 
-const filePath = path.join(__dirname, 'note.txt')
+fs.readFile("java.jpg", (err, data) => {
+    if (err) throw err;
 
-fs.appendFile(filePath, "\nthis is a new line", (err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log("data added successfully");
+    fs.writeFile("copy.png", data, () => {
+        console.log("image copied");
+    })
 })
