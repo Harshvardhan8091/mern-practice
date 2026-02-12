268cf7dce60bd3ccbd623289d386d8479dbcb0ae
Harshvardhan8091 <148524500+Harshvardhan8091@users.noreply.github.com>
2026-02-12 11:34:45 +0530

    mongo

--- MONGO/index.js
+++ MONGO/index.js
@@ -1,37 +1,40 @@
 import express from 'express'
 import mongoose from 'mongoose'
 
-const app = express();
+const app = express()
+
 mongoose.connect("mongodb://localhost:27017/ShopDB")
-    .then(()=> {console.log('database connection')
+.then(() => {
+    console.log("database connected")
+})
+.catch((err) => {
+    console.error("database error:", err)
+})
 
-    })
-    .catch((err)=>{
-        console.error('database error: ',err);
-    })
+const productSchema = new mongoose.Schema({
+    name: String,
+    price: Number
+})
 
-    const productSchema=new mongoose.Schema({
-        name: String,
-        price: Number
-    });
-
-const product = mongoose.model('Product', productSchema);
-
+const Product = mongoose.model("Product", productSchema)
 
 app.get("/products", async (req, res) => {
-    const page = parseInt(req.query.page) || 1;
+    const page = parseInt(req.query.page) || 1
+    const limit = 2
+    const skip = limit * (page - 1)
 
-    const limit = 2;
-    const skip = limit * (page - 1);
+    const products = await Product
+        .find({}, { name: 1, _id: 0 })
+        .sort({ price: 1 })
+        .skip(skip)
+        .limit(limit)
 
-    const product = await product.find({}, {name:1,_id:0}).sort({price:1}).skip(skip).limit(limit);
     res.json({
         page,
-        data: product
-    });
-    
-});
+        data: products
+    })
+})
 
 app.listen(3000, () => {
-    console.log('server is running on port 3000');
-}); 
+    console.log("server is running on port 3000")
+})
