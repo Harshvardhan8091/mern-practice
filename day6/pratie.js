0562856bed97508d1bc98cfd1641d5a7690a7e7e
Harshvardhan8091 <148524500+Harshvardhan8091@users.noreply.github.com>
2026-07-01 15:26:40 +0530

    js note

--- day6/pratie.js
+++ day6/pratie.js
@@ -18,3 +18,24 @@
 function login() {
     console.log("Login");
 }
+
+function getUser() {
+    console.log("Getting user...");
+    setTimeout(() => {
+        console.log("User details loaded");
+    }, 2000);
+}
+
+function getOrders() {
+    console.log("Getting orders...");
+    setTimeout(() => {
+        console.log("Orders loaded");
+    }, 3000);
+}
+
+function getOrderDetails() {
+    console.log("Getting order details...");
+    setTimeout(() => {
+        console.log("Order details loaded");
+    }, 4000);
+}
