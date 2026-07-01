062839968054cd9edadfab07bba78d43fed06849
Harshvardhan8091 <148524500+Harshvardhan8091@users.noreply.github.com>
2026-07-01 15:22:08 +0530

    js note

--- day6/pratie.js
+++ day6/pratie.js
@@ -2,3 +2,19 @@
 //getuser()
 //getorder()
 //getorderdetails()
+
+
+// callGreet(greet);
+// callGreet(greet("Jerry")); // we can't do this
+
+// callGreet(() => {
+//     greet("Harry");
+// })
+
+// setTimeout(() => {
+//     console.log("Hello");
+// }, 1000)
+
+function login() {
+    console.log("Login");
+}
