74c819938b66856c100d383c10ab0dfa972aa2ff
Harshvardhan8091 <148524500+Harshvardhan8091@users.noreply.github.com>
2026-07-02 14:42:40 +0530

    js note

--- day6/pratie.js
+++ day6/pratie.js
@@ -44,3 +44,6 @@ login();
 getUser();
 getOrders();
 getOrderDetails();
+
+
+one
---
+++ day7/pratie.js
@@ -0,0 +1,19 @@
+//A promise in java is an object that represt an eventual completion or failure of an
+ //asynchronous operation and its resulted value
+
+
+//it acts as a placeholder for a value which 
+//isn't available yet but will be available (resolved or rejected at some point of time
+
+const promise=new Promise((resolve, reject) => {
+    let success = true;
+setTimeout(() => {
+    if (success) {
+        resolve("Logged in");
+    }
+    else{
+        reject("Login failed");
+    
+    }
+},1000);
+});
