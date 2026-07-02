cb7083c89a6d358918386b1018170e4e2ce2f201
Harshvardhan8091 <148524500+Harshvardhan8091@users.noreply.github.com>
2026-07-02 15:16:12 +0530

    js note

--- day7/pratie.js
+++ day7/pratie.js
@@ -9,10 +9,10 @@ const promise=new Promise((resolve, reject) => {
     let success = true;
 setTimeout(() => {
     if (success) {
-        resolve("Logged in dash");
+        resolve("Logged in");
     }
     else{
-        reject("Login is failed");
+        reject("Login failed");
     
     }
 },1000);
@@ -23,5 +23,52 @@ promise.then(data => {
 }).catch(error => {
     console.error(error);
 }).finally(() => {
-    console.log("Login operation is completed.");
+    console.log("Login operation completed.");
 });
+
+
+
+function login() {
+    console.log("Login...");
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Logged in");
+        resolve();
+    }, 1000);
+})
+}
+
+function getUser() {
+    console.log("Getting user...");
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("User details loaded");
+            resolve();
+        }, 2000);
+    });
+}
+
+function getOrders() {
+    console.log("Getting orders...");
+    return new Promise((resolve, reject) => {
+    setTimeout(() => {
+        console.log("Orders loaded");
+        if (typeof cb === "function") cb();
+    }, 3000);
+})
+}
+
+function getOrderDetails() {
+    console.log("Getting order details...");
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Order details loaded");
+            resolve();
+        }, 4000);
+    });
+}
+
+login()
+.then(getUser)
+.then(getOrders)
+.then(getOrderDetails)
